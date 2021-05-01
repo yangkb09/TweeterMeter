1c6d7cc980b52f69f82a6859612c5483104659ad
Katarina Yang <yangkb09@gmail.com>
2021-05-01 14:20:03 -0400

    chore: delete commented out code

--- client/components/SentimentScore.js
+++ client/components/SentimentScore.js
@@ -5,45 +5,20 @@ import {_getScore} from '../store/form'
 export class SentimentScore extends React.Component {
   componentDidMount() {
     this.props.getScore()
-    // console.log('CDIDMOUNT this.props ', this.props)
-    // console.log('CDIDMOUNT this.state ', this.state)
   }
 
   render() {
-    // if (this.props.form[0]) {
-    //   {console.log('THIS.PROPS: ', this.props)}
-    //   {console.log('THIS.PROPS.FORM[0]: ', this.props.form[0])}
-    //   return (
-    //     <div className="card">
-    //       Analysis
-    //       {/* {console.log('RENDER this.props ', this.props)}
-    //       {console.log('RENDER this.state ', this.state)} */}
-    //       <div className="container">
-    //         Score: {this.props.form[0].score}
-    //         {/* {this.props.form.map(oneForm => {
-    //           return (
-    //             <div key={oneForm.id}>
-    //               Score: {oneForm.score}
-    //               magnitude: {oneForm.magnitude}
-    //             </div>
-    //           )
-    //         })} */}
-    //       </div>
-    //     </div>
-    //   )
-    // } else {
-    //   return <div>nothing here</div>
-    // }
     if (this.props) {
       {console.log('THIS.PROPS: ', this.props)}
-      {console.log('THIS.PROPS.FORM[0]: ', this.props.form[0])}
       return (
         <div className="card">
           Analysis
           {/* {console.log('RENDER this.props ', this.props)}
           {console.log('RENDER this.state ', this.state)} */}
           <div className="container">
+            {/* RENDER PROFILE PIC HERE */}
             Score: {this.props.form.score}
+            Magnitude: {this.props.form.magnitude}
             {/* {this.props.form.map(oneForm => {
               return (
                 <div key={oneForm.id}>

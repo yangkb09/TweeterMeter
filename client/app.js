1238f3702d91e9875f57dadf008a99b3f09973da
Katarina Yang <yangkb09@gmail.com>
2021-05-02 16:27:09 -0400

    refactor: remove need to be logged in to use app

--- client/app.js
+++ client/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import {Navbar} from './components'
+import UserHome from './components/user-home'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <UserHome />
       <Routes />
     </div>
   )
--- client/components/user-home.js
+++ client/components/user-home.js
@@ -12,7 +12,6 @@ export const UserHome = props => {
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
       <TextForm />
       <SentimentScore />
     </div>

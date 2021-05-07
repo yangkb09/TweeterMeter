db72fea52a6d4cd52afffb7ce97906dd0b5ae392
Katarina Yang <yangkb09@gmail.com>
2021-05-07 15:54:35 -0400

    chore: clean user-home.js component

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,19 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import TextForm from './TextForm'
 import SentimentScore from './SentimentScore'
 
-/**
- * COMPONENT
- */
-export const UserHome = props => {
-  const {email} = props
+export const UserHome = () => {
 
   return (
     <div className="gridContainer">
       <div id="title">TweeterMeter</div>
-      {/* <div id="twitterLogo"/> */}
       <img src="/images/twitterLogo.png" alt="White Twitter Logo" id="twitterLogo" />
       <TextForm />
       <SentimentScore />
@@ -21,20 +15,4 @@ export const UserHome = props => {
   )
 }
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    // email: state.user.email
-  }
-}
-
-export default connect(mapState)(UserHome)
-
-/**
- * PROP TYPES
- */
-UserHome.propTypes = {
-  email: PropTypes.string
-}
+export default connect(null)(UserHome)

e86704c9b0a0748cd37fcd7c7fd3e0987b2b2b26
Katarina Yang <yangkb09@gmail.com>
2021-05-07 13:06:05 -0400

    chore: remove unused routes

--- client/app.js
+++ client/app.js
@@ -1,14 +1,10 @@
 import React from 'react'
-
-import {Navbar} from './components'
 import UserHome from './components/user-home'
-import Routes from './routes'
 
 const App = () => {
   return (
     <div>
       <UserHome />
-      <Routes />
     </div>
   )
 }

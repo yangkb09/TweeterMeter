c339eeb880aa107e86cc415e329af38659b52167
Katarina Yang <yangkb09@gmail.com>
2021-05-07 12:09:52 -0400

    chore: delete unused code from server/index.js

--- server/index.js
+++ server/index.js
@@ -2,21 +2,9 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const compression = require('compression')
-const session = require('express-session')
-const passport = require('passport')
-// const SequelizeStore = require('connect-session-sequelize')(session.Store)
-// const db = require('./db')
-// const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
-const socketio = require('socket.io')
 module.exports = app
-
-// This is a global Mocha hook, used for resource cleanup.
-// Otherwise, Mocha v4+ never quits after tests.
-// if (process.env.NODE_ENV === 'test') {
-//   after('close the session store', () => sessionStore.stopExpiringSessions())
-// }
 
 /**
  * In your development environment, you can keep all of your
@@ -27,9 +15,6 @@ module.exports = app
  * Node process on process.env
  */
 if (process.env.NODE_ENV !== 'production') require('../secrets')
-
-// passport registration
-passport.serializeUser((user, done) => done(null, user.id))
 
 const createApp = () => {
   // logging middleware

918292f9952fab06d7719422c9caac8234434048
Katarina Yang <yangkb09@gmail.com>
2021-05-12 15:27:42 -0400

    feat: add analysis definitions, style: round banner corners

--- client/components/Instructions.js
+++ client/components/Instructions.js
@@ -4,21 +4,23 @@ import {connect} from 'react-redux'
 export const Instructions = () => {
   return (
     <div className="card" id="instructions">
-      <div className="container">
+      <div className="container, medText">
         Gauge the sentiment of twitter users through machine learning!
         <ol className="smallText">
           <li>Enter a Twitter Handle</li>
           <li>Click "Analyze"</li>
           <li>Let results generate</li>
         </ol>
-        {/* <div className="secondary">
-          <div>
-            Sentiment measures emotion on a scale from negative (-1.0) to positive (1.0)
-          </div>
-          <div>
-            Magnitude measures strength of emotion from 0 to infinity
-          </div>
-        </div> */}
+        <div className="smallText">
+          Analysis:
+          <ul>
+            <li>
+              Sentiment: emotion on a scale from negative (-1.0) to positive
+              (1.0)
+            </li>
+            <li>Magnitude: strength of emotion from 0 to infinity</li>
+          </ul>
+        </div>
       </div>
     </div>
   )

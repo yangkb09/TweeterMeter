bbde0b9c838c3e7cfde58823c72fc055055e6485
Katarina Yang <yangkb09@gmail.com>
2021-05-13 12:03:57 -0400

    chore: remove redux logger

--- client/store/index.js
+++ client/store/index.js
@@ -1,15 +1,13 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import formReducer from './form'
 
 const reducer = combineReducers({
-  form: formReducer,
+  form: formReducer
 })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
+
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware))
 const store = createStore(reducer, middleware)
 
 export default store
